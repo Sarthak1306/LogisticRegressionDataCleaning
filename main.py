b565f3b937cc81f07425ac4ded89f384e24368b0
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-24 15:14:07 +0530

    drop columns

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import pandas as pd 
+import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("data.csv")
+# print(data.head())
+# print(data.info())
+# print(data.describe())
+
+# sns.heatmap(data.isnull())
+# plt.show()
+
+data.drop(["Unnamed: 32", "id"], axis=1, inplace=True)
 print(data.head())
