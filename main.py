7efa98b6337e2ac0b3e462990d2c12b0e075e15a
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-24 15:25:58 +0530

    create get_clean_data function

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("data.csv")
-# print(data.head())
-# print(data.info())
-# print(data.describe())
 
-# sns.heatmap(data.isnull())
-# plt.show()
-
-data.drop(["Unnamed: 32", "id"], axis=1, inplace=True)
+def get_clean_data():
+    data = pd.read_csv("data.csv")
+    data.drop(["Unnamed: 32", "id"], axis=1, inplace=True)
+    data["diagnosis"] = data["diagnosis"].map({"M": 1, "B": 0})
+    return data
 
 
-data["diagnosis"] = data["diagnosis"].map({"M": 1, "B": 0})
+def main():
+    data = get_clean_data()
+
+    print(data.head())
 
 
-print(data.head())
+if __name__ == "__main__":
+    main()
