f8387796357dcd9a549dfa69db55a1d27c727665
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-24 15:36:53 +0530

    Split into train test

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+
+def create_model(data):
+    y = data["diagnosis"]  # target variable
+    X = data.drop(["diagnosis"], axis=1)
+
+    # Normalize the data using scikit scaler
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    # Split Data for Train,Test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_scaled, y, test_size=0.2, random_state=42
+    )
+
+    # Train the model
 
 
 def get_clean_data():
@@ -13,6 +33,7 @@ def get_clean_data():
 def main():
     data = get_clean_data()
 
+    scaler, model = create_model(data)
     print(data.head())
 
 
